d99ecb127a1a4f482eee4ace092187d7058a989b
Julien-Hashchak <2369740@cegepmontpetit.ca>
2026-02-24 15:52:51 -0500

    3

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -13,15 +13,31 @@ export default class Poutine {
     }
   }
   selectType(event) {
+    const boutonClick = event.currentTarget;
     for (let i = 0; i < this.types.length; i++) {
       const type = this.types[i];
       type.classList.remove('is-active');
-      event.currentTarget.classList.add('is-active');
+      boutonClick.classList.add('is-active');
+    }
+    if ('poulet' in boutonClick.dataset) {
+      console.log('adsg');
     }
     this.selectType = this.element.innerText;
     this.updatePhoto();
   }
-  updatePhoto() {
-    //console.log(this.element);
+
+  updatePhoto(event) {
+    const image = this.element.querySelector('img');
+    image.classList.add('is-active');
+
+    //for (let i = 0; i < this.types.length; i++) {
+    //  const type = this.types[i];
+    //  console.log(type);
+
+    // if (type.dataset.poulet) {
+    //   image.src = '../assets/images/poulet.png';
+    //   console.log('ajdgs');
+    // }
+    // }
   }
 }
